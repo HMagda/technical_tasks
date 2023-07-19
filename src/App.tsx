2092be4aaddafe22a2cda4e7aeb4d1615dea6a12
Magdalena <magdalenahyra@wp.pl>
2023-07-20 00:29:12 +0200

    Added flag icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import PhoneModal from './components/PhoneModal';
+import PhoneModal from './components/PhoneModal/PhoneModal';
+import './App.scss';
 
 type TestIdsFunction = () => string;
 type TestIdsValues = string | TestIdsFunction;
 
 type TestIdsType<T> = {
-  [P in keyof T]: 
-    T[P] extends (...args: any[]) => infer R ? (R extends string ? T[P] : TestIdsValues) :
-    T[P] extends object ? (Object extends T[P] ? TestIdsValues : TestIdsType<T[P]>) :
-    T[P] extends TestIdsValues ? T[P] : never;
+  [P in keyof T]: T[P] extends (...args: any[]) => infer R
+    ? R extends string
+      ? T[P]
+      : TestIdsValues
+    : T[P] extends object
+    ? Object extends T[P]
+      ? TestIdsValues
+      : TestIdsType<T[P]>
+    : T[P] extends TestIdsValues
+    ? T[P]
+    : never;
 };
 
 function typedFreeze<T extends TestIdsType<T>>(obj: T): Readonly<T> {
@@ -16,53 +24,44 @@ function typedFreeze<T extends TestIdsType<T>>(obj: T): Readonly<T> {
 }
 
 // This should pass
-const TEST_1 = typedFreeze({ 
+const TEST_1 = typedFreeze({
   key1: {
-    key2:  "example1", 
+    key2: 'example1',
     key3: {
-      key4: "example2"
-    }
-  }, 
-  key5:  () => "example3",
+      key4: 'example2',
+    },
+  },
+  key5: () => 'example3',
 } as const);
 
 // This shouldn't pass (will cause a compile error)
-// const TEST_2 = typedFreeze({ 
+// const TEST_2 = typedFreeze({
 //   key1: {
-//     key2:  "example1", 
+//     key2:  "example1",
 //     key3: {
 //       key4: "example2"
 //     }
-//   }, 
+//   },
 //   key5:  () => 10,
 // } as const);
 
 // This shouldn't pass (will cause a compile error)
-// const TEST_3 = typedFreeze({ 
+// const TEST_3 = typedFreeze({
 //   key1: {
-//     key2:  "example1", 
+//     key2:  "example1",
 //     key3: {
 //       key4: "example2"
 //     }
-//   }, 
+//   },
 //   key5:  {},
 // } as const);
 
 const App = () => {
-  const example1 = TEST_1.key1.key2;
-  const example2 = TEST_1.key1.key3.key4;
-  const example3 = TEST_1.key5();
-
   return (
-    <div className="App">
-      <h2>Test Results:</h2>
-      <p>{`Example1: ${example1}`}</p>
-      <p>{`Example2: ${example2}`}</p>
-      <p>{`Example3: ${example3}`}</p>
-      
+    <div className='App'>
       <PhoneModal />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/PhoneModal/PhoneModal.tsx
+++ src/components/PhoneModal/PhoneModal.tsx
@@ -1,20 +1,34 @@
 import React, {useState} from 'react';
 import ReactModal from 'react-modal';
-import Select from 'react-select';
+import Select, {OptionProps, components} from 'react-select';
 import {useQuery} from 'react-query';
+import {FlagIcon, FlagIconCode} from 'react-flag-kit';
 
 interface Country {
+  code: string;
   country_name: string;
   dialling_code: string;
 }
 
+interface SelectOption {
+  value: string;
+  label: string;
+  dial_code: string;
+  flag: string;
+}
+
+const CustomOption: React.FC<OptionProps<SelectOption, false>> = (props) => (
+  <components.Option {...props}>
+    <FlagIcon code={props.data.flag as FlagIconCode} />
+    <span>{props.data.label}</span>
+  </components.Option>
+);
+
 const PhoneModal = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [selectedCountry, setSelectedCountry] = useState<{
-    value: string;
-    label: string;
-    dialling_code: string;
-  } | null>(null);
+  const [selectedCountry, setSelectedCountry] = useState<SelectOption | null>(
+    null
+  );
   const [phoneNumber, setPhoneNumber] = useState('');
 
   const fetchCountryCodes = async () => {
@@ -22,10 +36,11 @@ const PhoneModal = () => {
       `http://apilayer.net/api/countries?access_key=${process.env.REACT_APP_NUMVERIFY_API_KEY}`
     );
     const data = await res.json();
+
     return Object.entries(data).map(([code, country]) => ({
       code,
       name: (country as Country).country_name,
-      dialling_code: (country as Country).dialling_code,
+      dial_code: (country as Country).dialling_code,
     }));
   };
 
@@ -33,6 +48,20 @@ const PhoneModal = () => {
     'countryCodes',
     fetchCountryCodes
   );
+
+  let options: SelectOption[] = [];
+
+  if (countryCodes) {
+    console.log('halo', countryCodes)
+    options = countryCodes
+      .filter((country) => country.code !== 'AN')
+      .map((country) => ({
+        value: country.name,
+        label: `${country.name} (${country.dial_code})`,
+        dial_code: country.dial_code,
+        flag: country.code,
+      }));
+  }
 
   const handleOpenModal = () => {
     setModalIsOpen(true);
@@ -45,6 +74,7 @@ const PhoneModal = () => {
   };
 
   const handleSave = () => {
+    // Add save functionality here.
     setModalIsOpen(false);
   };
 
@@ -56,22 +86,9 @@ const PhoneModal = () => {
     return <div>Error fetching data</div>;
   }
 
-  let options: {value: string; label: string; dialling_code: string}[] = [];
-
-  if (countryCodes) {
-    options = countryCodes.map(
-      (country: {code: string; name: string; dialling_code: string}) => ({
-        value: country.code,
-        label: `${country.name} (${country.dialling_code})`,
-        dialling_code: country.dialling_code,
-      })
-    );
-  }
-
   return (
-    <div>
+    <div className='modal-container'>
       <button onClick={handleOpenModal}>Change phone number</button>
-
       <ReactModal
         isOpen={modalIsOpen}
         style={{
@@ -83,21 +100,18 @@ const PhoneModal = () => {
         <Select
           options={options}
           value={selectedCountry}
-          onChange={(
-            option: {value: string; label: string; dialling_code: string} | null
-          ) => setSelectedCountry(option)}
+          onChange={(option) => setSelectedCountry(option)}
           isSearchable
           menuPortalTarget={document.body}
           styles={{menuPortal: (base) => ({...base, zIndex: 9999})}}
+          components={{Option: CustomOption}}
         />
-
         <input
           type='phone'
           placeholder='000-000-000'
           value={phoneNumber}
           onChange={(event) => setPhoneNumber(event.target.value)}
         />
-
         <button onClick={handleCancel}>Cancel</button>
         <button onClick={handleSave}>Save</button>
       </ReactModal>
